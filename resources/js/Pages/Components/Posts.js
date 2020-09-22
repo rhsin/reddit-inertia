2511a1cf6b6f9db1c4fe5440dc4dd44fdf83f1ab
Ryan-VM <ryanhsin@ymail.com>
2020-09-22 03:25:45 -0700

    added button to save post to DB, minor Tailwind CSS

--- resources/js/Pages/Components/Posts.js
+++ resources/js/Pages/Components/Posts.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import { fetchGroups } from './redux/actions';
 
 function Posts(props) {
-    const { users, groups } = props;
-    const [group, setGroup] = useState('news');
+    const { dispatch, users, groups } = props;
+    const [group, setGroup] = useState('webdev');
     const [posts, setPosts] = useState([]);
     const url = 'https://www.reddit.com';
+    const urlAPI = 'https://www.localhost.com/';
 
     useEffect(()=> {
         fetch(url + '/r/' + group + '.json')
@@ -14,7 +17,11 @@ function Posts(props) {
             const newPosts = data.data.children
                 .map(item => item.data);
             setPosts(newPosts);
-        });
+        })
+        .catch(err => {
+            console.error('Error: ', err);
+        }); 
+        dispatch(fetchGroups());
     }, [group]);
 
     const getDate = (time) => {
@@ -22,15 +29,25 @@ function Posts(props) {
         return date.toUTCString().slice(0, -7);
     };
 
+    const savePost = (post) => {
+        axios.post(urlAPI, {
+            title: post.title,
+            score: post.score,
+            link: post.permalink,
+            group_id: groups[1].id
+        });
+    };
+
     return (
         <>
-            <div>Posts Component</div>
+            <div className='bg-green-100'>Posts Component</div>
             <table>
-                <thead>
+                <thead className='table-auto'> 
                     <tr>
                         <th>Title</th>
                         <th>Score</th>
                         <th>Created</th>
+                        <th>Archive</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -46,7 +63,22 @@ function Posts(props) {
                             </td> 
                             <td>
                                 {getDate(item.created)}
-                            </td> 
+                            </td>
+                            <td>
+                                <button
+                                    className="bg-teal-100 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+                                    onClick={()=> savePost(item)}
+                                >
+                                    <svg
+                                        className="fill-current w-4 h-4 mr-2"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                    >
+                                        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+                                    </svg>
+                                    <span>Save</span>
+                                </button> 
+                            </td>
                         </tr>
                     )}
                 </tbody>
