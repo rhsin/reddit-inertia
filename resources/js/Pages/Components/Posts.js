9c693872062fbab264f68484018dbdf43e2e46d9
rhsin <ryanhsin@ymail.com>
2020-09-22 18:49:32 -0700

    trying out Chakra UI components & hooks

--- resources/js/Pages/Components/Posts.js
+++ resources/js/Pages/Components/Posts.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import {
+    FormControl,
+    FormLabel,
+    Button,
+    Select,
+    useToast,
+    Stack,
+    Box,
+    Heading,
+    Text,
+    Badge
+} from '@chakra-ui/core';
 
 function Posts(props) {
     const { users, groups } = props;
 
-    const [group, setGroup] = useState('webdev');
+    const [group, setGroup] = useState('PS4');
     const [posts, setPosts] = useState([]);
+
+    const toast = useToast();
 
     const url = 'https://www.reddit.com';
     const urlAPI = 'http://localhost:8000/';
@@ -35,6 +49,14 @@ function Posts(props) {
         .catch(err => {
             console.log(err);
         });
+        toast({
+            position: 'top',
+            title: 'Post Archived!',
+            description: 'We saved this post for you',
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+        });
     };
 
     const getDate = (time) => {
@@ -45,9 +67,10 @@ function Posts(props) {
     return (
         <>
             <div className='bg-green-100'>Posts Component</div>
-            <div className="inline-block relative w-64">
-                <select
-                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            <FormControl>
+                <FormLabel htmlFor='subreddit'>Subreddit</FormLabel>
+                <Select
+                    id='subreddit'
                     onChange={e => setGroup(e.target.value)}
                 >
                     {groups.map(item => 
@@ -55,55 +78,43 @@ function Posts(props) {
                             {item.name}
                         </option>
                     )}
-                </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
-                    </svg>
-                </div>
-            </div>
-            <table>
-                <thead className='table-auto'> 
-                    <tr>
-                        <th>Title</th>
-                        <th>Score</th>
-                        <th>Created</th>
-                        <th>Archive</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {posts.map((item, index) => index < 10 &&
-                        <tr key={item.id}>
-                            <td>
-                                <a href={url + item.permalink}>
-                                    {item.title}
-                                </a>
-                            </td> 
-                            <td>
+                </Select>
+            </FormControl>
+            <Stack spacing={3}>
+                {posts.map((item, index) => index < 10 &&
+                    <Box
+                        p={5}
+                        shadow='md'
+                        borderWidth='1px'
+                        key={item.id}
+                    >
+                        <Heading fontSize='lg' mb='2'>
+                            <a href={url + item.permalink}>
+                                {item.title}
+                            </a>
+                        </Heading>
+                        <Badge variantColor='blue' mr='2'>
+                            <Text fontSize="md">
                                 {item.score.toLocaleString()}
-                            </td> 
-                            <td>
+                            </Text>
+                        </Badge> 
+                        <Badge mr='2'>
+                            <Text fontSize="xs">
                                 {getDate(item.created)}
-                            </td>
-                            <td>
-                                <button
-                                    className="bg-teal-100 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-                                    onClick={()=> savePost(item)}
-                                >
-                                    <svg
-                                        className="fill-current w-4 h-4 mr-2"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
-                                    </svg>
-                                    <span>Save</span>
-                                </button> 
-                            </td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+                            </Text>
+                        </Badge>
+                        <Button
+                            leftIcon='download'
+                            variantColor='teal'
+                            variant='outline'
+                            size='sm'
+                            onClick={()=> savePost(item)}
+                        >
+                            Save
+                        </Button> 
+                    </Box>
+                )}
+            </Stack>
         </>
     );
 }
--- resources/js/Pages/Home.js
+++ resources/js/Pages/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './Components/redux/store';
 import Dashboard from './Components/Dashboard';
+import { ThemeProvider } from '@chakra-ui/core'
 
 function Home(props) {
     return (
         <Provider store={store}>
-            <Dashboard posts={props.posts.data}/>
+            <ThemeProvider>
+                <Dashboard posts={props.posts.data}/>
+            </ThemeProvider>
         </Provider>
     );
 }
